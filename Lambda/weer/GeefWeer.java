98b7cef11a49fa3cfd8c2bdea1ce727824faceb3
develop <bomaltennishal.@outlook.com>
2025-07-25 16:43:29 +0200

    ll

--- Lambda/weer/GeefWeer.java
+++ Lambda/weer/GeefWeer.java
@@ -3,12 +3,14 @@ package Lambda.weer;
 public class GeefWeer {
 
     public String geefVoorspelling(String voorspelling) {
-        WeerInterface wi = (w) -> "Voorspelling: " + w;
+//        WeerInterface wi = (w) -> {if (w.equals("Zon")) return " Vreselijk"; else return "Te doen";};
+        WeerInterface wi = (w) -> (w.equals("Zon") ? "Vreselijk" : "Te doen");
+//        reverse ? (a, b) -> a - b : (a, b) -> b - a;
         return wi.getWeer(voorspelling);
     }
 
 
     public static void main(String[] args) {
-        System.out.println(new GeefWeer().geefVoorspelling("Zoiets"));
+        System.out.println(new GeefWeer().geefVoorspelling("Zop"));
     }
 }
